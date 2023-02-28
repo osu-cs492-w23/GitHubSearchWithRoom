8c68b909f19e7ff78baa52a1f7fe7b1ab1a2f715
Rob Hess <hessro@oregonstate.edu>
2023-02-28 15:34:31 -0800

    Add code from 2/28 lecture.

--- app/src/main/java/com/example/githubsearchwithroom/data/GitHubRepo.kt
+++ app/src/main/java/com/example/githubsearchwithroom/data/GitHubRepo.kt
@@ -1,11 +1,19 @@
 package com.example.githubsearchwithroom.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
+@Entity
 data class GitHubRepo(
-    @Json(name = "full_name") val name: String,
+    @Json(name = "full_name")
+    @PrimaryKey
+    val name: String,
+
     @Json(name = "html_url") val url: String,
     val description: String?,
     @Json(name = "stargazers_count") val stars: Int
